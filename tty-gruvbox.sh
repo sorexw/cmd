9a16af05fa3b4b9990d4ccaa26e1563c4a1d19a0
sorex <ehfazinfo@gmail.com>
2026-06-13 20:01:44 +0600

    Update tty-gruvbox.sh

--- tty-gruvbox.sh
+++ tty-gruvbox.sh
@@ -1,1 +1,1 @@
-echo -en "\e]P0282828\e]P1cc241d\e]P298971a\e]P3d65d0e\e]P4458588\e]P5b16286\e]P6689d6a\e]P7a89984\e]P8928374\e]P9fb4934\e]PAb8bb26\e]PBfabd2f\e]PC83a598\e]PDd3869b\e]PE8ec07c\e]PFebdbb2" && clear
+echo -en "\e]P032302f\e]P1fb4934\e]P2b8bb26\e]P3fabd2f\e]P483a598\e]P5d3869b\e]P68ec07c\e]P7ebdbb2\e]P8665c54\e]P9fe8019\e]PAbdae93\e]PBebdbb2\e]PC458588\e]PDb16286\e]PE689d6a\e]PFa89984" && clear
